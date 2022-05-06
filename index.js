12ac0dcbf3cc6fced7a250d0b36199e57ba56769
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-05-07 00:39:48 +0600

    delete operation add

--- index.js
+++ index.js
@@ -60,6 +60,14 @@ async function run() {
 
         })
 
+        //delete data from item
+        app.delete("/items/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result)
+        })
+
     } finally {
         //await client.close();
     }
